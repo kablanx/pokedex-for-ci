3c3cb0d09d11767e72ed83d43300656aafa2a206
Sebastian P <sperez@stemdo.io>
2023-05-04 14:15:08 +0200

    prueba 3

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ const mapResults = (({ results }) => results.map(({ url, name }) => ({
   name,
   id: parseInt(url.match(/\/(\d+)\//)[1])
 })))
-/* prueba 2*/
+/* prueba 3*/
 const App = () => {
   const { data: pokemonList, error, isLoading } = useApi('https://pokeapi.co/api/v2/pokemon/?limit=784', mapResults)
   if (isLoading) {
