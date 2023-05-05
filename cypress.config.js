24cedf14e06141d74f6bb972c5e58d04c44da22e
Sebastian P <sperez@stemdo.io>
2023-05-05 13:42:15 +0200

    another try 3

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,19 @@
 /* global cy */
-describe('Pokedex', function(){
-  it('frontpage can be opened', function(){
-    cy.visit('http::/localhost:5000')
-    cy.contains('Pikachu')
-    cy.contains('Pokemon and Pokemon character names are trademarks of Nintendo')
-  })
+const { defineConfig } = require('cypress')
+
+module.exports = defineConfig({
+  baseUrl: 'http://localhost:1234',
+  tests: [
+    {
+      describe: 'Pokedex',
+      it: 'frontpage can be opened',
+      test: function() {
+        cy.visit('http://localhost:5000')
+        cy.contains('Pikachu')
+        cy.contains('Pokemon and Pokemon character names are trademarks of')
+      }
+    }
+  ]
 })
+
 
