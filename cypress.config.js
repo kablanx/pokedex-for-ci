4e4ee424a3e4464eacd4c416d43e08e3d02eb408
Sebastian P <sperez@stemdo.io>
2023-05-05 13:22:04 +0200

    Fixed cypress.config.js

--- cypress.config.js
+++ cypress.config.js
@@ -1,5 +1,13 @@
+/* global cy */
+
 module.exports = {
   video: false
 }
-
+describe('Pokedex', function(){
+  it('frontpage can be opened', function(){
+    cy.visit('http::/localhost:5000')
+    cy.contains('Pikachu')
+    cy.contains('Pokemon and Pokemon character names are trademarks of Nintendo')
+  })
+})
 
